1bf65f9581d40479f9e1d4dd1c501fdbeeda3eca
BugaevAV <iustusvir@gmail.com>
2025-03-19 17:15:11 +0300

    initial commit

---
+++ src/main/java/ru/javabegin/micro/demo/countryidentification/CountryIdentificationApplication.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.demo.countryidentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryIdentificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryIdentificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javabegin/micro/demo/countryidentification/CountryIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.micro.demo.countryidentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryIdentificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
